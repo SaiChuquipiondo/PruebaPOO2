6754b2dd141c50550bc5171e47709c7699e31038
jairowong <133531948+jairowong@users.noreply.github.com>
2024-10-18 17:44:30 -0500

    Modificado

--- src/main/java/com/model/AutoresModel.java
+++ src/main/java/com/model/AutoresModel.java
@@ -7,10 +7,12 @@ import com.beans.Autor;
 
 public class AutoresModel {
 
-	public List<Autor> autores(){
+	public List<Autor> ListarAutores(){
 		ArrayList<Autor> autor = new ArrayList<>();
-		
-		return null;
+		 autor.add(new Autor(1,"Gracia Marquez","Colombiana"));
+		 autor.add(new Autor(2,"Maria Torre","Peruana"));
+		 autor.add(new Autor(3,"Jose Diaz","Argentina"));
+		return autor;
 		
 	}
 }
