1799dea3a9cd3f01419abe5189a1773ab410cac8
SaiChuquipiondo <saichuquipiondopinchi@gmail.com>
2024-10-17 22:52:15 -0500

    Agregue clases autores

---
+++ src/main/java/com/model/AutoresModel.java
@@ -0,0 +1,16 @@
+package com.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.beans.Autor;
+
+public class AutoresModel {
+
+	public List<Autor> autores(){
+		ArrayList<Autor> autor = new ArrayList<>();
+		
+		return null;
+		
+	}
+}
